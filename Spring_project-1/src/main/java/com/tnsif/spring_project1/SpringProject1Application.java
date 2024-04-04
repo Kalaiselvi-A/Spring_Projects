e3938a4c836be6bc1211e893bb33c39a34f72760
kalai135 <kalaiarumugham135@gmail.com>
2024-04-04 22:41:30 +0530

    Spring Boot Project #1

--- Spring_project-1/src/main/java/com/tnsif/spring_project1/SpringProject1Application.java
+++ Spring_project-1/src/main/java/com/tnsif/spring_project1/SpringProject1Application.java
@@ -8,6 +8,7 @@ public class SpringProject1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProject1Application.class, args);
+		System.out.println("Hello Connections....!");
 	}
 
 }
